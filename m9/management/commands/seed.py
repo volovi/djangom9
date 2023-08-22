f99a5bfea95d3eaa488e095bb4a63b6372ac7539
Alexei Volovikov <alexei.volovikov@gmail.com>
2023-08-22 20:18:29 +0300

    Format the code with autopep8 and pylint

--- m9/management/commands/seed.py
+++ m9/management/commands/seed.py
@@ -2,11 +2,13 @@ from django.core.management.base import BaseCommand
 from m9.models import MagicSquare
 from ._private import generate
 
+
 class Command(BaseCommand):
     help = "Generates the 9 x 9 pan-magic squares and seeds the database."
 
     def add_arguments(self, parser):
-        parser.add_argument('-s', '--symmetric_only', action='store_true', help="Keep symmetric magic squares only.")
+        parser.add_argument('-s', '--symmetric_only', action='store_true',
+                            help="Keep symmetric magic squares only.")
 
     def handle(self, *args, **options):
         for values, hint in generate(options["symmetric_only"]):
